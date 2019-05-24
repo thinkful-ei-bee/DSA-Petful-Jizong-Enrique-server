57de13580db1ed9a69e895901077c59f69bf4c7e
JizongL <2015rpro@gmail.com>
2019-05-24 13:52:55 -0400

    added cat

---
+++ Petful-server/src/AnimalData/cat.js
@@ -0,0 +1,64 @@
+const cat = [
+  {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+},
+{
+  imageURL:'https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/08/scottish-fold-detail.jpg?bust=1535567146&width=355', 
+  imageDescription: 'Scottish Fold Cat with orange eyes',
+  name: 'Aldo',
+  sex: 'Male',
+  age: 3,
+  breed: 'Scottish Fold Cat',
+  story: 'given away by previous owner'
+},
+{
+  imageURL:'https://cdn3-www.cattime.com/assets/uploads/2011/12/file_2710_Egyptian-Mau-cat-breed.jpg', 
+  imageDescription: 'Egyptian Mau Cat with yellow eyes and black and white pattern',
+  name: 'Murphy',
+  sex: 'Female',
+  age: 1,
+  breed: 'Egyptian Mau Cat',
+  story: 'Thrown on the street'
+},
+{
+  imageURL:'https://bengalcatworld.com/wp-content/uploads/2013/04/Tabby-Cat-vs-Bengal-Cat-1.jpg', 
+  imageDescription: 'Orange bengal cat with brown strips',
+  name: 'Mochi',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'given away by owner'
+},
+{
+  imageURL:'https://vignette.wikia.nocookie.net/cats/images/9/94/Sokoke_dalili.jpg/revision/latest?cb=20110217220541&path-prefix=en', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fiona',
+  sex: 'Female',
+  age: 3,
+  breed: 'Sokoke',
+  story: 'Thrown on the street'
+},
+{
+  imageURL:'https://vignette.wikia.nocookie.net/warriors-ocs/images/1/15/Berryeye.jpg/revision/latest?cb=20180306022226', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Teddy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Tabby',
+  story: 'abandoned'
+},
+{
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+}]
