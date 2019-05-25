56b662afb7495e5aa4cfa8b641d6a5ce34030292
Enrique Montemayor <eemontemayor@live.com>
2019-05-24 19:38:01 -0500

    tests are passing

--- Petful-server/test/app.spec.js
+++ Petful-server/test/app.spec.js
@@ -1,9 +1,38 @@
 const app = require('../src/app')
+const supertest = require('supertest');
 
 describe('App', () => {
-  it('GET / responds with 200 "Hello, world!"', () => {
+  it('GET /cats responds with 200', () => {
     return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
+      .get('/api/cats')
+      .expect(200)
   })
+  it('GET /dogs responds with 200', () => {
+    return supertest(app)
+      .get('/api/dogs')
+      .expect(200)
+  })
+  it('GET /people responds with 200', () => {
+    return supertest(app)
+      .get('/api/people')
+      .expect(200)
+  })
+  it('DELETE /people responds with 200', () => {
+    return supertest(app)
+      .delete('/api/people')
+      .expect(202)
+  })
+  it('DELETE /cats responds with 200', () => {
+    return supertest(app)
+      .delete('/api/cats')
+      .expect(202)
+  })
+  it('DELETE /dogs responds with 200', () => {
+    return supertest(app)
+      .delete('/api/dogs')
+      .expect(202)
+  })
+
+
 })
+
